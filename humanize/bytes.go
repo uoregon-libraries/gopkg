0dd6fed634f02fc0e0bed180eb3a12d7deff4e8e
Jeremy Echols <jechols@uoregon.edu>
2017-12-21 11:12:07 -0800

    Add humanize.Bytes for filesize calculations

---
+++ humanize/bytes.go
@@ -0,0 +1,117 @@
+package humanize
+
+import (
+	"fmt"
+	"strconv"
+)
+
+// Values for different suffixes.  We only need up to exabytes, as anything
+// larger can't be stored in a uint64.  And, at least currently, exabytes are
+// absurdly large anyway.
+const (
+	Kilobyte = 1024
+	Megabyte = Kilobyte * 1024
+	Gigabyte = Megabyte * 1024
+	Terabyte = Gigabyte * 1024
+	Petabyte = Terabyte * 1024
+	Exabyte  = Petabyte * 1024
+)
+
+// Bytes returns a human-friendly value for filesizes
+func Bytes(bytes uint64) string {
+	var divVal uint64
+	var suffix string
+	var decToStr = func(val uint64) string { return strconv.FormatUint(val, 10) }
+
+	switch {
+	case bytes >= Exabyte:
+		divVal = Exabyte
+		suffix = " EB"
+
+	case bytes >= Petabyte:
+		divVal = Petabyte
+		suffix = " PB"
+
+	case bytes >= Terabyte:
+		divVal = Terabyte
+		suffix = " TB"
+
+	case bytes >= Gigabyte:
+		divVal = Gigabyte
+		suffix = " GB"
+
+	case bytes >= Megabyte:
+		divVal = Megabyte
+		suffix = " MB"
+
+	case bytes >= Kilobyte:
+		divVal = Kilobyte
+		suffix = " KB"
+
+	default:
+		// As a special case, when we have no need for division, we just build a
+		// simple string inline
+		return decToStr(bytes) + " B"
+	}
+
+	var whole = bytes / divVal
+	var dec = ((bytes % divVal) / (divVal / 1024) * 100) / 1024
+	switch {
+	case dec == 0:
+		return decToStr(whole) + suffix
+
+	case dec < 10:
+		return decToStr(whole) + ".0" + decToStr(dec) + suffix
+
+	// This can happen with huge numbers that should be rounded up
+	case dec > 99:
+		whole++
+		return decToStr(whole) + suffix
+
+	default:
+		return decToStr(whole) + "." + decToStr(dec) + suffix
+	}
+}
+
+// bytesSimple isn't intended for use; it's just built to show why we don't use
+// the simpler approach of using floats and fmt.Sprintf - benchmarks put this
+// function taking 2.5x-7x as long as the above function, where 2.5x is only
+// seen when the bytes are < 1024, which in practice is likely to be uncommon.
+func bytesSimple(bytes uint64) string {
+	var val float64
+	var size = float64(bytes)
+	var suffix string
+
+	switch {
+	case bytes >= Exabyte:
+		val = size / Exabyte
+		suffix = " EB"
+
+	case bytes >= Petabyte:
+		val = size / Petabyte
+		suffix = " PB"
+
+	case bytes >= Terabyte:
+		val = size / Terabyte
+		suffix = " TB"
+
+	case bytes >= Gigabyte:
+		val = size / Gigabyte
+		suffix = " GB"
+
+	case bytes >= Megabyte:
+		val = size / Megabyte
+		suffix = " MB"
+
+	case bytes >= Kilobyte:
+		val = size / Kilobyte
+		suffix = " KB"
+
+	default:
+		// As a special case, when we have no need for division, we just build a
+		// simple string inline
+		return fmt.Sprintf("%d B", bytes)
+	}
+
+	return fmt.Sprintf("%0.2f%s", val, suffix)
+}
---
+++ humanize/bytes_test.go
@@ -0,0 +1,53 @@
+package humanize
+
+import (
+	"math"
+	"testing"
+)
+
+// TestBytes verifies various values get converted correctly
+func TestBytes(t *testing.T) {
+	var valueExpectations = []struct {
+		size uint64
+		str  string
+	}{
+		{size: 50, str: "50 B"},
+		{size: 1023, str: "1023 B"},
+		{size: 1024, str: "1 KB"},
+		{size: Terabyte - Gigabyte, str: "1023 GB"},
+		{size: Terabyte - 1, str: "1023.99 GB"},
+		{size: Terabyte, str: "1 TB"},
+		{size: Terabyte + Megabyte, str: "1 TB"},
+		{size: 5000000000, str: "4.65 GB"},
+		{size: math.MaxUint64, str: "15.99 EB"},
+	}
+
+	for _, expect := range valueExpectations {
+		var actual = Bytes(expect.size)
+		if actual != expect.str {
+			t.Errorf("%d should have given us %q, but instead we got %q", expect.size, expect.str, actual)
+		}
+	}
+}
+
+func BenchmarkBytes(b *testing.B) {
+	var tests = []uint64{0, 1, 2, 3, 4, 1024, 2048, 5000, 5000000000000, math.MaxUint64, 5000000}
+	tests = []uint64{2048, 5000, 5000000000000, math.MaxUint64, 5000000}
+	b.ResetTimer()
+
+	b.Run("Bytes", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			for _, t := range tests {
+				Bytes(t)
+			}
+		}
+	})
+
+	b.Run("bytesSimple", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			for _, t := range tests {
+				bytesSimple(t)
+			}
+		}
+	})
+}
