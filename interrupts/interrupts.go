ae0761eda0adf48ab84882c09d9196a8aec55a2b
Jeremy Echols <jechols@uoregon.edu>
2017-12-26 11:01:57 -0800

    Remove unnecessary type information

--- interrupts/interrupts.go
+++ interrupts/interrupts.go
@@ -12,7 +12,7 @@ import (
 var isDone int32
 
 // Logger is set to the default logger, but can be overridden for custom behavior
-var Logger *logger.Logger = logger.DefaultLogger
+var Logger = logger.DefaultLogger
 
 // TrapIntTerm catches interrupt and termination signals to let processes exit
 // more cleanly.  A second signal of either type will immediately end the
