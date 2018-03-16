44313d6ffc8c603d3ad0b571b04d4ea88908026e
Jeremy Echols <jechols@uoregon.edu>
2018-03-16 11:17:07 -0700

    fileutil: Improve SafeFile example

--- fileutil/example_safe_file_test.go
+++ fileutil/example_safe_file_test.go
@@ -7,32 +7,36 @@ import (
 	"github.com/uoregon-libraries/gopkg/fileutil"
 )
 
-func makeWrite(fname string) *fileutil.SafeFile {
-	var f = fileutil.NewSafeFile(fname)
-	f.Write([]byte("This is a test.\n\nA what?\n\nA test.\n\nA what?\n\nA test.\n\nOh, a test.\n"))
-	return f
-}
-
 func Example_minimal() {
+	var testOut = []byte("This is a test.\n\nA what?\n\nA test.\n\nA what?\n\nA test.\n\nOh, a test.\n")
 	var fname = "/tmp/blah.txt"
-	var f = makeWrite(fname)
+	var f = fileutil.NewSafeFile(fname)
+	f.Write(testOut)
+
+	// At this point there's no data in the file
+	var data, err = ioutil.ReadFile(fname)
+	fmt.Printf("data: %q; err: %q\n\n", data, err)
 
 	// Closing twice shouldn't cause problems
 	f.Close()
 	f.Close()
 
-	var data, err = ioutil.ReadFile(fname)
+	// Now that the SafeFile has been closed, we have data
+	data, err = ioutil.ReadFile(fname)
 	if err != nil {
-		fmt.Printf("Error trying to read %q: %s", fname, err)
+		fmt.Printf("Error trying to read %q: %s\n\n", fname, err)
 	}
 	fmt.Println(string(data))
 
-	f = makeWrite(fname)
+	f = fileutil.NewSafeFile(fname)
+	f.Write(testOut)
 	f.Cancel()
 	data, err = ioutil.ReadFile(fname)
 	fmt.Println(err)
 
 	// Output:
+	// data: ""; err: "open /tmp/blah.txt: no such file or directory"
+	//
 	// This is a test.
 	//
 	// A what?
