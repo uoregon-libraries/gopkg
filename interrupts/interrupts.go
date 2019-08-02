49acc587bfd1822c1a5bedac6c50489bb7bfd762
Jeremy Echols <jechols@uoregon.edu>
2019-08-02 07:55:08 -0700

    interrupts: fix default logger name

--- interrupts/interrupts.go
+++ interrupts/interrupts.go
@@ -12,7 +12,7 @@ import (
 var isDone int32
 
 // Logger is set to a default STDERR logger, but can be overridden for custom behavior
-var Logger = logger.Named("gopkg/pdf.ImageDPIs", logger.Debug)
+var Logger = logger.Named("gopkg/interrupts", logger.Debug)
 
 // TrapIntTerm catches interrupt and termination signals to let processes exit
 // more cleanly.  A second signal of either type will immediately end the
