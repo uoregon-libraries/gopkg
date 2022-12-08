b30d3c51f7a0514ec958cc63ac233c5b74fbe4c2
Jeremy Echols <jechols@uoregon.edu>
2022-12-08 09:03:16 -0800

    bagit: add full filesystem-based test

--- bagit/bagit_test.go
+++ bagit/bagit_test.go
@@ -2,6 +2,7 @@ package bagit
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
 	"path/filepath"
 	"sort"
@@ -36,5 +37,50 @@ func TestGenerateChecksums(t *testing.T) {
 
 	for i, ck := range b.Checksums {
 		assert.Equal(expectedChecksums[i], ck.Checksum, "checksum for "+ck.Path, t)
+	}
+}
+
+func TestWriteTagFiles(t *testing.T) {
+	var wd, err = os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+
+	var path = filepath.Join(wd, "testdata")
+	os.Remove(filepath.Join(path, "manifest-sha256.txt"))
+	os.Remove(filepath.Join(path, "tagmanifest-sha256.txt"))
+	os.Remove(filepath.Join(path, "bagit.txt"))
+	var b = New(path)
+	err = b.WriteTagFiles()
+	if err != nil {
+		t.Fatalf("error generating checksums in %q: %s", b.root, err)
+	}
+
+	var fname = "manifest-sha256.txt"
+	var raw []byte
+	raw, err = ioutil.ReadFile(filepath.Join(path, fname))
+	if err != nil {
+		t.Fatalf("error reading %q: %s", fname, err)
+	}
+	var got = string(raw)
+	var expected = `60fa80b948a0acc557a6ba7523f4040a7b452736723df20f118d0aacb5c1901b  data/another.txt
+55f8718109829bf506b09d8af615b9f107a266e19f7a311039d1035f180b22d4  data/test.txt
+`
+	if expected != got {
+		t.Fatalf("Expected %q to be %q, but got %q", fname, expected, raw)
+	}
+
+	fname = "tagmanifest-sha256.txt"
+	raw, err = ioutil.ReadFile(filepath.Join(path, fname))
+	if err != nil {
+		t.Fatalf("error reading %q: %s", fname, err)
+	}
+	got = string(raw)
+	expected = `157add7a6600f47a8149b9eab2b35370300f54a73475ded76694078eec5a77df  .gitignore
+e91f941be5973ff71f1dccbdd1a32d598881893a7f21be516aca743da38b1689  bagit.txt
+e24a952af486ce42a2119d89bec8c7a8c42c2ae9e6302efce5833cf381775594  manifest-sha256.txt
+`
+	if expected != got {
+		t.Fatalf("Expected %q to be %q, but got %q", fname, expected, raw)
 	}
 }
